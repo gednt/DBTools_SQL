095a3f5bca4c701a20c4246e20e189746142e460
Felipe Coelho R. Silva <reisfelipe18@hotmail.com>
2021-12-23 09:56:14 -0300

    Code Reorganization

---
+++ DBTools/Controller/DBToolsController.cs
@@ -0,0 +1,36 @@
+﻿using DBTools.Model;
+using DBToolsDll;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBTools.Controller
+{
+    public class DBToolsController : Interfaces.IDBTools
+    {
+        public DBToolsDll.DBTools_SQL DBTools = new DBToolsDll.DBTools_SQL();
+
+        public DBToolsController(DBToolsDll.DBTools_SQL dbTools)
+        {
+            DBTools = dbTools;
+        }
+
+        public DataView RetrieveDataSQL()
+        {
+            return DBTools.RetrieveDataSql();
+        }
+
+        public List<GenericObject> RetrieveObjectSQL()
+        {
+            return DBTools.RetrieveObjectSql();
+        }
+
+        public void SqlExecuteQuery(String query = "")
+        {
+            DBTools.SqlExecuteQuery();
+        }
+    }
+}
